b1fb1c1ca2ee46afbf7701e1d785cd82e3dc96af
Marco <marcoasjunior90@gmail.com>
2020-03-30 16:14:11 -0300

    commit inicial

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import React from 'react';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+        <Switch>
+            {/* <Route path='/'exact component={Logon} /> */}
+        </Switch>
+        </BrowserRouter>
+    )
+}
